f2267d103d15f74d7258a22f7370e1b14cb90943
Ninolito <ninolitopeligrino@gmail.com>
2025-12-19 07:22:30 +0000

    refs/heads/main bfba2149c8ed5d4d72443ee5eac456271bd6d4b5

--- amor/v1/amor.pb.go
+++ amor/v1/amor.pb.go
@@ -873,7 +873,7 @@ type UpdateAccommodationRequest struct {
 	Amenities     []string                        `protobuf:"bytes,4,rep,name=amenities,proto3" json:"amenities,omitempty"`
 	Description   *string                         `protobuf:"bytes,5,opt,name=description,proto3,oneof" json:"description,omitempty"`
 	Rules         []string                        `protobuf:"bytes,6,rep,name=rules,proto3" json:"rules,omitempty"`
-	ProfilePhoto  []byte                          `protobuf:"bytes,7,opt,name=profile_photo,json=profilePhoto,proto3,oneof" json:"profile_photo,omitempty"`
+	ProfilePic    []byte                          `protobuf:"bytes,7,opt,name=profile_pic,json=profilePic,proto3,oneof" json:"profile_pic,omitempty"`
 	unknownFields protoimpl.UnknownFields
 	sizeCache     protoimpl.SizeCache
 }
@@ -950,9 +950,9 @@ func (x *UpdateAccommodationRequest) GetRules() []string {
 	return nil
 }
 
-func (x *UpdateAccommodationRequest) GetProfilePhoto() []byte {
+func (x *UpdateAccommodationRequest) GetProfilePic() []byte {
 	if x != nil {
-		return x.ProfilePhoto
+		return x.ProfilePic
 	}
 	return nil
 }
@@ -2175,17 +2175,18 @@ const file_amor_v1_amor_proto_rawDesc = "" +
 	"\n" +
 	"_photo_url\"?\n" +
 	"\x12UpdateRoomResponse\x12)\n" +
-	"\x04room\x18\x01 \x01(\v2\x15.accumora_rpc.v1.RoomR\x04room\"\xad\x02\n" +
+	"\x04room\x18\x01 \x01(\v2\x15.accumora_rpc.v1.RoomR\x04room\"\xa7\x02\n" +
 	"\x1aUpdateAccommodationRequest\x12\x0e\n" +
 	"\x02id\x18\x01 \x01(\tR\x02id\x12\x12\n" +
 	"\x04name\x18\x02 \x01(\tR\x04name\x12D\n" +
 	"\x04type\x18\x03 \x01(\x0e20.accumora_rpc.v1.Accommodation.AccommodationTypeR\x04type\x12\x1c\n" +
 	"\tamenities\x18\x04 \x03(\tR\tamenities\x12%\n" +
 	"\vdescription\x18\x05 \x01(\tH\x00R\vdescription\x88\x01\x01\x12\x14\n" +
-	"\x05rules\x18\x06 \x03(\tR\x05rules\x12(\n" +
-	"\rprofile_photo\x18\a \x01(\fH\x01R\fprofilePhoto\x88\x01\x01B\x0e\n" +
-	"\f_descriptionB\x10\n" +
-	"\x0e_profile_photo\"\x10\n" +
+	"\x05rules\x18\x06 \x03(\tR\x05rules\x12$\n" +
+	"\vprofile_pic\x18\a \x01(\fH\x01R\n" +
+	"profilePic\x88\x01\x01B\x0e\n" +
+	"\f_descriptionB\x0e\n" +
+	"\f_profile_pic\"\x10\n" +
 	"\x0eGetUserRequest\"\xb8\x02\n" +
 	"\x0eAddRoomRequest\x12\x12\n" +
 	"\x04name\x18\x01 \x01(\tR\x04name\x12)\n" +
